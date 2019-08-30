eaad35fdffc84e81c3515b52612ec6beab89edc8
rami554 <menendez.ticona.ramiro@gmail.com>
2019-08-29 21:07:49 -0400

    Primer Commit - Fibonacci

---
+++ src/Prac1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+public static void Fibonacci {
+
+int n = 0, t1 = 0, t2 = 1;
+System.out.println("Cuantas posiciones desea mostrar en la serie?");
+Scanner scanner = new Scanner(System.in);
+n = scanner.nextInt();
+System.out.println("Hasta " + n + "posiciones: ");
+while (int i = 1; i<=n; i++)
+{
+
+System.out.print(t1+" ");
+int sum = t1 + t2;
+t2 = sum;
+
+}
+}
+}
